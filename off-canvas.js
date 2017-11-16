d38724474e7bc5de2ab01ffec1ac11c3e6438661
Grant Eagon <geagon@gmail.com>
2017-11-16 16:07:17 -0500

    add strict dependency injection

--- off-canvas.js
+++ off-canvas.js
@@ -17,54 +17,57 @@
 	function cnOffCanvas(angular) {
 
 		angular.module('cn.offCanvas', [])
-        	.factory('cnOffCanvas', function($compile, $rootScope, $controller, $http, $templateCache, $q) {
-        		return function (config) {
-        			var container = angular.element(config.container || document.body),
-        				containerClass = config.containerClass || 'is-off-canvas-opened',
-        				controller = config.controller || angular.noop,
-        				controllerAs = config.controllerAs,
-        				element = null,
-        				html, deferred, scope, ctrl;
+        	.factory('cnOffCanvas', cnOffCanvasFactory);
 
-        			if ((+!!config.template) + (+!!config.templateUrl) !== 1) {
-        				throw new Error('You must specify either a `template` or a `templateUrl` to create an off-canvas navigation.');
-        			}
+		cnOffCanvasFactory.$inject = ['$compile', '$rootScope', '$controller', '$http', '$templateCache', '$q'];
 
-        			if (config.template) {
-        				deferred = $q.defer();
-        				deferred.resolve(config.template);
-        				html = deferred.promise;
-        			} else {
-        				html = $http.get(config.templateUrl, {
-        					cache: $templateCache
-        				}).then(function(response) {
-        					return response.data;
-        				});
-        			}
+		function cnOffCanvasFactory ($compile, $rootScope, $controller, $http, $templateCache, $q) {
+    		return function (config) {
+    			var container = angular.element(config.container || document.body),
+    				containerClass = config.containerClass || 'is-off-canvas-opened',
+    				controller = config.controller || angular.noop,
+    				controllerAs = config.controllerAs,
+    				element = null,
+    				html, deferred, scope, ctrl;
 
-        			html.then(function(html) {
-        				scope = $rootScope.$new();
-        				ctrl = $controller(controller, {$scope: scope});
-        				if (controllerAs) {
-        					scope[controllerAs] = ctrl;
-        				}
-        				element = angular.element(html);
-        				container.prepend(element);
-        				$compile(element)(scope);
-        			})
+    			if ((+!!config.template) + (+!!config.templateUrl) !== 1) {
+    				throw new Error('You must specify either a `template` or a `templateUrl` to create an off-canvas navigation.');
+    			}
 
-        			function toggle() {
-        				this.isOpened = !this.isOpened;
-        				container.toggleClass(containerClass);
-        			}
+    			if (config.template) {
+    				deferred = $q.defer();
+    				deferred.resolve(config.template);
+    				html = deferred.promise;
+    			} else {
+    				html = $http.get(config.templateUrl, {
+    					cache: $templateCache
+    				}).then(function(response) {
+    					return response.data;
+    				});
+    			}
 
-        			return {
-        				toggle: toggle,
-        				isOpened: false
-        			}
-        		}
-        	});
+    			html.then(function(html) {
+    				scope = $rootScope.$new();
+    				ctrl = $controller(controller, {$scope: scope});
+    				if (controllerAs) {
+    					scope[controllerAs] = ctrl;
+    				}
+    				element = angular.element(html);
+    				container.prepend(element);
+    				$compile(element)(scope);
+    			})
 
+    			function toggle() {
+    				this.isOpened = !this.isOpened;
+    				container.toggleClass(containerClass);
+    			}
+
+    			return {
+    				toggle: toggle,
+    				isOpened: false
+    			}
+    		}
+    	}
 		return 'cn.OffCanvas';
 	}
 
